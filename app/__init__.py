f1709c9ca84db2faa6bb5175401536c76b269fdc
Josh Hall <joshhall90@yahoo.com>
2019-03-27 12:06:06 -0400

    Added flask_cors to fix the heroku CORS error

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,13 @@ from flask import Flask
 from config import Config
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-# from flask_cors import CORS
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+CORS(app)
 
 # bootstrap = Bootstrap(app)
 
